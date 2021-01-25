9090a48a820099435b3b85480f6139b7e6f7eed2
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-01-25 17:50:27 +0900

    Add files via upload

---
+++ balance.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:34:58 2021
+
+@author: mi-nakada
+"""
+
+# ライブラリの読み込み
+import pandas as pd
+
+# テストデータの読み込み
+test = pd.read_csv('test.tsv', sep='\t')
+
+# テストデータの中身を確認する
+print(test.head())
+print(test.dtypes)
+
+# 'id'の削除と確認
+test = test.drop(test.columns[[0]], axis=1)
+print(test.head())
+print(len(test))
+
+# testデータのclassを予測
+preds = [] # 予測値を格納する空のリストを準備
+
+for i in range(len(test)):
+    l_wd = test.iloc[i,0] * test.iloc[i,1] # left_weight * left_distance
+    r_wd = test.iloc[i,2] * test.iloc[i,3] # right_weight * right_distance
+    
+    if l_wd > r_wd: 
+        preds.append(0)
+        
+    elif l_wd == r_wd:
+        preds.append(1)
+    
+    elif l_wd < r_wd:
+        preds.append(2)
+
+'''
+提出
+'''
+
+# 提出用データの読み込み
+sub = pd.read_csv('sample_submit.csv', sep=',', header=None)
+print(sub.head())
+    
+# 目的変数カラムの置き換え
+sub[1] = preds
+
+# ファイルのエクスポート
+sub.to_csv('submission.csv', header=None, index = None)
---
+++ balance_EDA.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 13:53:07 2021
+
+@author: mi-nakada
+"""
+
+# ライブラリの読み込み
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+# データの読み込み
+train = pd.read_csv('train.tsv', sep='\t')
+
+# データの確認
+print(train.head())
+
+# 'id'の削除と確認
+train = train.drop(train.columns[[0]], axis=1)
+print(train.head())
+print(train.shape)
+
+print(train.iloc[0,0])
+
+# trainデータのclassを予測
+y_train_preds = []
+
+for i in range(len(train)):
+    l_wd = train.iloc[i,1] * train.iloc[i,2] # left_weight * left_distance
+    r_wd = train.iloc[i,3] * train.iloc[i,4] # right_weight * right_distance
+    
+    if l_wd > r_wd:
+        y_train_preds.append(0)
+        
+    elif l_wd == r_wd:
+        y_train_preds.append(1)
+    
+    elif l_wd < r_wd:
+        y_train_preds.append(2)
+
+# trainデータのclass（目的変数）を取得
+y_train_true = train['class']
+
+# accuracyを計算する
+score = accuracy_score(y_train_true, y_train_preds)
+print(score)
